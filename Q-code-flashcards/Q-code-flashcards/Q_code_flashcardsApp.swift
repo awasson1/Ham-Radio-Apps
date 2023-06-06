6f53bee24130c630490b87f638ffa0a9764b4539
awasson1 <47997743+awasson1@users.noreply.github.com>
2023-06-05 22:30:53 -0700

    Q-code flashcard project creation

---
+++ Q-code-flashcards/Q-code-flashcards/Q_code_flashcardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Q_code_flashcardsApp.swift
+//  Q-code-flashcards
+//
+//  Created by Anthony Wasson on 6/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Q_code_flashcardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
